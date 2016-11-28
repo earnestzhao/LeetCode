32359b1d23a250e5450f396e070f75749752bb84
Earnest <earnest001@163.com>
2016-11-28 17:53:25 +0800

    460. Init

---
+++ LeetCode_460.cpp
@@ -0,0 +1,19 @@
+class LFUCache
+{
+public:
+
+    LFUCache(int capacity)
+    {
+        
+    }
+    
+    int get(int key)
+    {
+        
+    }
+    
+    void set(int key, int value)
+    {
+        
+    }
+};
